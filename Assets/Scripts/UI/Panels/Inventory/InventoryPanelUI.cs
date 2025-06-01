792a57e143bfce0a9bf5d61589c8204a3eb357ff
Andree Burlamaqui <umdreeb@gmail.com>
2025-06-01 05:05:41 -0300

    Updating inventory correctly during runtime

--- Assets/Scripts/UI/Panels/Inventory/InventoryPanelUI.cs
+++ Assets/Scripts/UI/Panels/Inventory/InventoryPanelUI.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +9,9 @@ public class InventoryPanelUI : BasePanelUI
     [SerializeField] private BaseGrid<ItemSO> itemGrid;
     [SerializeField] private InventorySlotUI slotUI;
 
+    InventorySO inventory;
+    List<InventorySlotUI> slots = new();
+
     private void Start()
     {
         if(panelSO is InventorySO inventorySO)
@@ -16,24 +20,46 @@ public class InventoryPanelUI : BasePanelUI
         }
     }
 
+    private void OnEnable()
+    {
+        Refresh();
+    }
+
     private void SetupInventory(InventorySO inventorySO)
     {
+        inventory = inventorySO;
+
         // Setup grid
         itemGrid = new BaseGrid<ItemSO>(2, 4, 1);
         for (int i = 0; i < itemGrid.Width * itemGrid.Height; i++)
         {
+            // Get slot UI and item if any
             var newSlot = Instantiate(slotUI, layout.transform);
-            var itemSlot = i < inventorySO.items.Count ? inventorySO.items[i] : null;
-            newSlot.Setup(itemSlot);
+            slots.Add(newSlot);
         }
 
-        Refresh(inventorySO);
+        Refresh();
     }
 
 
-    private void Refresh(InventorySO inventorySO)
+    private void Refresh()
     {
+        if(inventory == null)
+        {
+            return;
+        }
 
-    
+        for (int s = 0; s < slots.Count; s++)
+        {
+            var itemSlot = s < inventory.items.Count ? inventory.items[s] : null;
+
+            // Set it to the slot UI
+            slots[s].Setup(itemSlot);
+
+            // Set the item on the grid
+            int x = s % itemGrid.Width;
+            int y = s / itemGrid.Width;
+            itemGrid.SetValue(x, y, itemSlot);
+        }
     }
 }
